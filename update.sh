3aeba6f3e94576b59c84e69d9e1f4367b291d51a
initialencounter <2911583893@qq.com>
2025-06-15 21:18:49 +0800

    fix(ci)

--- update.sh
+++ update.sh
@@ -1,6 +1,3 @@
-#! /usr/bin/env nix-shell
-#! nix-shell -i bash --pure --keep GITHUB_TOKEN -p nix git curl cacert nix-prefetch-git jq
-
 if [ "$1" = "napcat" ]; then
     version=$(curl "https://api.github.com/repos/NapNeko/NapCatQQ/releases/latest" | jq -r '.tag_name')
     amd64_url="https://github.com/NapNeko/NapCatQQ/releases/download/$version/NapCat.Shell.zip"
