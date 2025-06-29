ffe76cadf0f64e99977d50eb0e53be0cf16a818f
initialencounter <2911583893@qq.com>
2025-06-29 11:32:42 +0800

    feat: precise version control for qq

--- update.sh
+++ update.sh
@@ -12,16 +12,20 @@ if [ "$1" = "napcat" ]; then
 fi
 
 if [ "$1" = "qq" ]; then
-    payload=$(curl https://im.qq.com/rainbow/linuxQQDownload | grep -oP "var params= \K\{.*\}(?=;)")
-    amd64_url=$(jq -r .x64DownloadUrl.deb <<< "$payload")
-    arm64_url=$(jq -r .armDownloadUrl.deb <<< "$payload")
-    version=$(jq -r .version <<< "$payload")-$(jq -r .updateDate <<< "$payload")
+    url=$2
 
-    # use friendlier hashes
+    hash=$(echo "$url" | grep -oP '/QQNT/\K[^/]+')
+    version=$(echo "$url" | grep -oP 'linuxqq_\K[^_]+')
+
+    amd64_url="https://dldir1v6.qq.com/qqfile/qq/QQNT/${hash}/linuxqq_${version}_amd64.deb"
+    arm64_url="https://dldir1v6.qq.com/qqfile/qq/QQNT/${hash}/linuxqq_${version}_arm64.deb"
+
     amd64_hash=$(nix-prefetch-url $amd64_url)
     arm64_hash=$(nix-prefetch-url $arm64_url)
-    amd64_hash=$(nix hash convert --hash-algo sha256 "$amd64_hash")
-    arm64_hash=$(nix hash convert --hash-algo sha256 "$arm64_hash").
+
+    # use friendlier hashes
+    amd64_hash=$(nix hash convert --to sri --hash-algo sha256 "$amd64_hash")
+    arm64_hash=$(nix hash convert --to sri --hash-algo sha256 "$arm64_hash")
     
     sed -i "s|# Last updated: .*\.|# Last updated: $(date +%F)\.|g" ./src/sources.nix
     sed -i "s|qq_version = \".*\";|qq_version = \"$version\";|g" ./src/sources.nix
